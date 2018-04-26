64e9b8a57eb1364196f5a0a294f60d63927c9926
Brandon Geren <brandongeren@gmail.com>
2018-04-25 22:50:32 -0500

    Add basic distribution visualization

---
+++ visualization.py
@@ -0,0 +1,12 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# df is a dataframe of amazon rating 5-core data
+# the data is in the format as read in by the code available on the ucsd website
+def plot_distribution(df, colname, filename):
+	ratings = df['overall'].value_counts().sort_index()
+	ratings = ratings.to_frame()
+	ratings['index'] = ratings.index
+	ax = sns.barplot(x="index", y="overall", data=ratings)
+	plt.savefig(filename)
