ae40b589c586cb2066f2f27171d5c02fb0bff755
Brandon Geren <brandongeren@gmail.com>
2018-04-26 23:02:07 -0500

    Generalize visualization

--- visualization.py
+++ visualization.py
@@ -5,8 +5,8 @@ import pandas as pd
 # df is a dataframe of amazon rating 5-core data
 # the data is in the format as read in by the code available on the ucsd website
 def plot_distribution(df, colname, filename):
-	ratings = df['overall'].value_counts().sort_index()
+	ratings = df[colname].value_counts().sort_index()
 	ratings = ratings.to_frame()
 	ratings['index'] = ratings.index
-	ax = sns.barplot(x="index", y="overall", data=ratings)
+	ax = sns.barplot(x="index", y=colname, data=ratings)
 	plt.savefig(filename)
