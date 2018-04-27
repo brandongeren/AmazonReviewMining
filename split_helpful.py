80821915c81e6fbf88bfae089fc3dad7e820cf58
Brandon Geren <brandongeren@gmail.com>
2018-04-26 21:21:23 -0500

    Changed some whitespace

--- split_helpful.py
+++ split_helpful.py
@@ -2,12 +2,9 @@ import pandas as pd
 import gzip
 
 def split_helpful(data):
-	data['helpful']=data['helpful'].str.replace('[','')
-	data['helpful']=data['helpful'].str.replace(']','')
-	data['helpful']=data['helpful'].str.replace(',','')
-
-	data[['helped','totalhelp']] = data.helpful.str.split(' ',expand=True)
-
-	del data['helpful']
-	
-return data
+  data['helpful']=data['helpful'].str.replace('[','')
+  data['helpful']=data['helpful'].str.replace(']','')
+  data['helpful']=data['helpful'].str.replace(',','')
+  data[['helped','totalhelp']] = data.helpful.str.split(' ',expand=True)
+  del data['helpful']	
+  return data
