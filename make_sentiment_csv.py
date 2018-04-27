346898690f8fa5f09ba6cd929be4590bd8bc44f3
Brandon Geren <brandongeren@gmail.com>
2018-04-26 21:40:06 -0500

    Make a runnable script for getting sentiment df

---
+++ make_sentiment_csv.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from SentimentAnalysis_Vader import * 
+from import_as_pandas import getDF
+from split_helpful import *
+from argparse import ArgumentParser
+
+parser.add_argument('filename', type=str, help="filename for Amazon data")
+
+def make_csv(filename):
+  df = getDF(filename)
+  df_with_sentiment = df[['reviewText', 'asin', 'overall', 'unixReviewTime', 'helpful', 'reviewerID']]
+  df_with_sentiment = add_sentiment_column(df_with_sentiment)
+  df_with_sentiment.drop('compound')
+  df_with_sentiment.drop('reviewText')
+  df_with_sentiment = split_helpful(df_with_sentiment)
+  df_with_sentiment.to_csv(filename.split('.')[1] + '.csv', index=False)
+  return df_with_sentiment
+
+make_csv(args.filename)
